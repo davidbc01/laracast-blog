b39d0987a2bb9e2e17f5c3355a8c0ad1521c88d1
David <dbartolc01@iescastelar.com>
2024-01-09 19:57:03 +0100

    Añado todo el proyecto del curso de Laracast (laravel 8 from scratch) hasta el episodio 17 - 09-01-2024

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('posts/{post}', function ($slug) {
+    return view('post', [
+        'post' => Post::findOrFail($slug)
+    ]);
+});
