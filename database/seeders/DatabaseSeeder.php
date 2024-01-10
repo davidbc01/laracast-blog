43c2b92f81ffd7b8494291447a83ffc75e8f910b
David <dbartolc01@iescastelar.com>
2024-01-10 15:02:20 +0100

    Añado avances al proyecto desde el episodio 18 hasta el 38 - 10-01-2024

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        $user = User::factory()->create([
+            'name'=>'John Doe'
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
