1df7910769a085308a9fc699a8a54bccf69ce6fd
David <dbartolc01@iescastelar.com>
2024-01-11 15:14:28 +0100

    Añado avances al proyecto desde el episodio 52 hasta el 55

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -49,6 +49,32 @@
                         {!! $post->body !!}
                     </div>
                 </div>
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-panel>
+                        <form method="POST" action="#">
+                            @csrf
+
+                            <header class="flex items-center">
+                                <img src="https://i.pravatar.cc/100?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
+
+                                <h2 class="font-bold text-xl ml-4">Want to participate?</h2>
+                            </header>
+
+                            <div class="mt-6">
+                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" cols="30" rows="5" placeholder="Quick, thing of something to say!"></textarea>
+                            </div>
+
+                            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                            </div>
+                        </form>
+                    </x-panel>
+
+                    @foreach($post->comments as $comment)
+                        <x-post-comment :comment="$comment" />
+                    @endforeach
+                </section>
             </article>
         </main>
     </section>
