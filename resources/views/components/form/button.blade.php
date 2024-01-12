22af20a29cb1841ab1fa176bedf6ec8a98202489
David <davidbc.18k@gmail.com>
2024-01-12 15:10:39 +0100

    Arreglo errores para que el programa funcione bien tanto visual para el usuario como no

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -1,4 +1,6 @@
-<button type="submit"
-        class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
-    {{ $slot }}
-</button>
+<x-form.field>
+    <button type="submit"
+            class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
+        {{ $slot }}
+    </button>
+</x-form.field>
--- resources/views/components/form/field.blade.php
+++ resources/views/components/form/field.blade.php
@@ -1,3 +1,3 @@
-<div class="mb-6">
+<div class="mt-6">
     {{ $slot }}
 </div>
